307047fd66c788fd3ec49072ddd3e17873fcdf86
Code Sandbox <hello@codesandbox.io>
2022-11-28 03:18:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.28d57716086d612673c0c0d8affcb254.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dcd05e76dc96c891d8b",
+    "url": "https://duckieplayz.github.io/csb-e2c0qy/static/css/main.70224b70.chunk.css"
+  },
+  {
+    "revision": "6dcd05e76dc96c891d8b",
+    "url": "https://duckieplayz.github.io/csb-e2c0qy/static/js/main.6dcd05e7.chunk.js"
+  },
+  {
+    "revision": "3e372fd9785e9ca12056",
+    "url": "https://duckieplayz.github.io/csb-e2c0qy/static/css/1.b2b07814.chunk.css"
+  },
+  {
+    "revision": "3e372fd9785e9ca12056",
+    "url": "https://duckieplayz.github.io/csb-e2c0qy/static/js/1.3e372fd9.chunk.js"
+  },
+  {
+    "revision": "33c3155dfdb912b44b0d",
+    "url": "https://duckieplayz.github.io/csb-e2c0qy/static/js/runtime~main.33c3155d.js"
+  },
+  {
+    "revision": "f008f705c7f00970794d97a897e2f832",
+    "url": "https://duckieplayz.github.io/csb-e2c0qy/static/media/applogo.f008f705.PNG"
+  },
+  {
+    "revision": "a51709efa283c69dd8bf1bb5412acc50",
+    "url": "https://duckieplayz.github.io/csb-e2c0qy/index.html"
+  }
+];
